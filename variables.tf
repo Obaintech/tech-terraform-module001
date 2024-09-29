b5bbc71b837468b087e1a3874b7dc53a2a996197
Obaintech <obadara574@gmail.com>
2024-09-29 20:41:26 +0100

    module commit001

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  description = "The environment to deploy into (e.g., Production, Staging)."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region to deploy the resources."
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account."
+  type        = string
+}
